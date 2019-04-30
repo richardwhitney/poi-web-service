3727853a3f44f86461f1421b2517d15716717f98
Richard Whitney <rwhitney32@gmail.com>
2019-04-30 21:22:14 +0100

    added findOne method of points api

--- app/api/points.js
+++ app/api/points.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Point = require('../models/poi');
+const Boom = require('boom');
 
 const Points = {
 
@@ -11,6 +12,21 @@ const Points = {
       return points;
     }
   },
+
+  findOne: {
+    auth: false,
+    handler: async function(request, h) {
+      try {
+        const point = await Point.findOne({ _id: request.params.id });
+        if (!point) {
+          return Boom.notFound('No point with this id');
+        }
+        return point;
+      } catch (e) {
+        return Boom.notFound('No point with this id');
+      }
+    }
+  }
 };
 
 module.exports = Points;
--- routesapi.js
+++ routesapi.js
@@ -1,5 +1,6 @@
 const Points = require('./app/api/points');
 
 module.exports = [
-  { method: 'GET', path: '/api/points', config: Points.find }
+  { method: 'GET', path: '/api/points', config: Points.find },
+  { method: 'GET', path: '/api/points/{id}', config: Points.findOne }
 ];
